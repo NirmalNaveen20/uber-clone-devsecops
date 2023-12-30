2b1a369150af772ee00fdc6c007cb85435ca3c51
= <53178630+NirmalNaveen20@users.noreply.github.com>
2023-12-30 19:51:42 +0530

    Test fix

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -34,7 +34,7 @@ data "aws_subnets" "public" {
 }
 #cluster provision
 resource "aws_eks_cluster" "example" {
-  name     = "eks_"
+  name     = "eks_cloud1120"
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
