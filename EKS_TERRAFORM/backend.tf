cc4a5317e01b53b2c698a14ee49024a376afe1af
= <53178630+NirmalNaveen20@users.noreply.github.com>
2023-12-30 19:24:10 +0530

    Update config

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "uber1120" # Replace with your actual S3 bucket name
+    bucket = "uber-devsecops20" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
